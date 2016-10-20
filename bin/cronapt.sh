622d6453be08937cdb98a7dc966b18d22be455e1
TsT <tst2005@gmail.com>
2016-10-20 13:44:05 +0200

    support to be run from user-space

--- bin/cronapt.sh
+++ bin/cronapt.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#set -e
+cd -- "$(dirname "$0")/.." || exit 123
+
 #TODO: gerer l'envoie d'email en cas d'info voulue
 #TODO: pouvoir utiliser les memes fonctions mais changer l'affichage final ?
 
@@ -79,14 +82,18 @@ cronapt_putloglevel() {
 cronapt_default() {
 	local r=$?
 	case "$action" in
-		start) cronapt_putloglevel debug "$context..." ;;
-		action) ;;
-		stop)	cronapt_putloglevel debug "$context done." ;;
-		filter) ;;
-		result)
+		(start) cronapt_putloglevel debug "$context..." ;;
+		(action) ;;
+		(stop)	cronapt_putloglevel debug "$context done." ;;
+		(filter) ;;
+		(result)
 			cronapt_putloglevel debug "$context (returned code $r) got:"
 			local level=${level:-debug}
 			cronapt_putlog_result_with_prefix "$context:"
+		;;
+		(*)
+			echo >&2 "ERROR: Unknown action $action"
+			return 1
 		;;
 	esac
 	return $?
@@ -137,9 +144,11 @@ cronapt_hook() {
 	return $r;
 }
 cronapt() {
-	local context=$1;shift
+	local context="$1";shift
 	local result;
 	local ERROR=0
+	cronapt_load "$context" || return 1
+
 	cronapt_hook start "$@";
 	result="$(cronapt_hook action "$@")"
 	ERROR=$?
@@ -151,93 +160,19 @@ cronapt() {
 
 cronapt_isroot() { [ "$(id -nu)" = "root" ]; }
 
-
-
-cronapt_action_errortest() { nonexistantcommand; }
-cronapt_filter_errortest() { grep -o 'not found'; }
-cronapt_result_errortest() {
-	echo "custom result handler here, but also call the default one"
-	cronapt_default
+cronapt_load() {
+	. "./commands/$1"
 }
 
-cronapt_action_update() { if cronapt_isroot; then cronapt_aptget -o quiet=2 update; fi; }
-#cronapt_action_update() { echo >&2 "fake-update raise an error on stderr"; echo "fake-update raise an error on stdout"; false; }
-#cronapt_filter_update() { cat; }
-cronapt_result_update() {
-	if [ $ERROR -ne 0 ]; then
-		level=error
-		#echo "oh shit the apt-get update failed! we need to raise a fucking error :D"
-		cronapt_default
-		return 1
-	fi
+main() {
+	LOGLEVEL="${*:-alert error}"
+	cronapt update
+#	cronapt autoclean -s
+#	cronapt download
+	cronapt available4upgrade upgrade
+#	cronapt available4upgrade dist-upgrade
+#	cronapt simul_upgrade
 }
-
-cronapt_action_autoclean() { cronapt_aptget -o quiet=1 autoclean -y "$@"; }
-cronapt_filter_autoclean() { cronapt_lessverbose; }
-
-cronapt_action_download() { cronapt_aptget -o quiet=1 dist-upgrade -d -y -o APT::Get::Show-Upgraded=true; }
-cronapt_filter_download() { cronapt_changeonly | cronapt_drop0change; }
-cronapt_result_download() {
-	cronapt_default
-	if [ -n "$result" ]; then
-		cronapt_putloglevel alert "$context: something available ($result)"
-	else
-		cronapt_putloglevel info "$context: Nothing to update"
-	fi
-}
-
-cronapt_action_simul_upgrade() { cronapt_aptget upgrade -qq -s -o APT::Get::Show-Upgraded=true; }
-cronapt_filter_simul_upgrade() { cronapt_lessverbose | grep -o 'Inst [^ ]*'; }
-cronapt_result_simul_upgrade() {
-	if [ -n "$result" ]; then
-		level=info
-		cronapt_default
-	fi
-}
-cronapt_action_available4upgrade() {
-	if [ -z "$1" ]; then
-		echo "Usage: available4upgrade <dist-upgrade|upgrade>"
-		return 1
-	fi
-	cronapt_aptget ${1:-upgrade} -s -o quiet=1 -o APT::Get::Show-Upgraded=true;
-}
-cronapt_filter_available4upgrade() { cronapt_changeonly | cronapt_drop0change; }
-cronapt_result_available4upgrade() {
-	if [ -n "$result" ]; then
-		level=alert
-		local shortresult="$(printf '[~%s/+%s/-%s]%0.0s\n' $(cronapt_result_to_upg_new_rem_kep "$result"))"
-		echo "$result | mail -s 'CRON-APT.sh: $context.$1: update(s) available(s) $shortresult' \$MAILTO"
-		#timestampformat='no' cronapt_putlog_result_with_prefix "$1:"
-	fi
-}
-
-
-
-
-LOGLEVEL="${*:-alert error}"
-
-# ( LOGLEVEL='' cronapt errortest >/dev/null ) && echo ca fail pas ?!
-
-cronapt update
-# cronapt autoclean -s
-# cronapt download
-cronapt available4upgrade upgrade
-cronapt available4upgrade dist-upgrade
-# cronapt simul_upgrade
-
+main "$@"
 exit $?
-
-#cronapt_errortest() {
-#	local result;
-#	result="$(
-#		(
-#			ERROR=0
-#			nonexistantcommand || ERROR=1
-#			# other command || ERROR=1
-#			exit $ERROR
-#		) 2>&1
-#	)"
-#	return $?
-#}
-
 
